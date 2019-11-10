01ec8bf4b2b99331e6051f02fdfb0e143a455df2
root <root@localhost.localdomain>
2019-11-10 16:17:41 +0700

    Latihan Mengirim Data Antar Fragment

--- app/src/main/java/com/svafvel/software/production/latihanfragmentandroid/CategoryFragment.java
+++ app/src/main/java/com/svafvel/software/production/latihanfragmentandroid/CategoryFragment.java
@@ -3,6 +3,7 @@ package com.svafvel.software.production.latihanfragmentandroid;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
@@ -32,26 +33,38 @@ public class CategoryFragment extends Fragment implements View.OnClickListener{
     }
 
     @Override
-    public void onViewCreated(@Nullable View view, @Nullable Bundle savedInstanceState){
-
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
         Button btnDetailCategory = view.findViewById(R.id.btn_detail_category);
         btnDetailCategory.setOnClickListener(this);
-
-
     }
+
 
     @Override
     public void onClick(View v) {
         if(v.getId() == R.id.btn_detail_category){
 
-            CategoryFragment mCategoryFragment = new CategoryFragment();
+            DetailCategoryFragment mDetailCategoryFragment = new DetailCategoryFragment();
+
+            Bundle mBundle = new Bundle();
+            mBundle.putString(DetailCategoryFragment.EXTRA_NAME, "Lifestyle");
+            String description = "Katagori ini akan berisi produk-produk lifestyle";
+
+            mDetailCategoryFragment.setArguments(mBundle);
+            mDetailCategoryFragment.setDescription(description);
+
+            /* Method addBackStack akan menambahkan fragment ke backstact Behavior dari button akan cek
+            * fragmane dari backstact, jika ada fragment di dalam backstack maka fragment yang akan di
+            * closse / remove jika sudah tidak ada fragment di dalam backstack maka activity yang akan diclose / finish */
+
+
+//            CategoryFragment mCategoryFragment = new CategoryFragment();
             FragmentManager mFragmentManager = getFragmentManager();
             if(mFragmentManager != null){
 
                 mFragmentManager.
                         beginTransaction().
-                        replace(R.id.frame_container, mCategoryFragment, CategoryFragment.class.getSimpleName()).
+                        replace(R.id.frame_container, mDetailCategoryFragment, DetailCategoryFragment.class.getSimpleName()).
                         addToBackStack(null).
                         commit();
 
